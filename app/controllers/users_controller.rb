49536ff9a7cc7529b44b1b43e12e165f4baee53a
YoritakaImata <yoimata@hb.tp1.jp>
2016-07-30 07:13:41 +0000

    add location

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
-  
+  before_action :set_params, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def show
-    @user = User.find(params[:id])
+    
   end
   
   def new
@@ -18,9 +20,29 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Update Your Profile"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
   
+  def set_params
+    @user = User.find(params[:id])
+  end
+
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :location)
+  end
+  
+  def correct_user
+    redirect_to root_path if @user != current_user
   end
 end
