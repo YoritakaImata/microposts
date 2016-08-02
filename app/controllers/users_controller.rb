77846ead020037d5df6c0086156cdb64eaa8d94d
YoritakaImata <yoimata@hb.tp1.jp>
2016-08-02 11:38:12 +0000

    add posting microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_params, only: [:show, :edit, :update]
-  before_action :correct_user, only: [:edit, :update]
-
+  
   def show
-    
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
   end
   
   def new
@@ -20,29 +19,9 @@ class UsersController < ApplicationController
     end
   end
   
-  def edit
-  end
-  
-  def update
-    if @user.update(user_params)
-      flash[:success] = "Update Your Profile"
-      redirect_to @user
-    else
-      render 'edit'
-    end
-  end
-  
   private
   
-  def set_params
-    @user = User.find(params[:id])
-  end
-
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :location)
-  end
-  
-  def correct_user
-    redirect_to root_path if @user != current_user
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
